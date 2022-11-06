42cda76c3f2e6e8b164b7a09ad1792a79388b8f0
Griza20 <amargrizovic5@gmail.com>
2022-11-06 21:21:09 +0100

    Initial commit

---
+++ src/main/java/ba/unsa/etf/rpr/Laptop.java
@@ -0,0 +1,88 @@
+package ba.unsa.etf.rpr;
+
+/**
+ * Hello world!
+ *
+ */
+public class Laptop {
+    private String brend;
+    private String model;
+    private String procesor;
+    private String grafickaKartica;
+    private double cijena, velicinaEkrana;
+    private int ram, hdd, ssd;
+
+    public String getBrend() {
+        return brend;
+    }
+
+    public void setBrend(String brend) {
+        this.brend = brend;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getProcesor() {
+        return procesor;
+    }
+
+    public void setProcesor(String procesor) {
+        this.procesor = procesor;
+    }
+
+    public String getGrafickaKartica() {
+        return grafickaKartica;
+    }
+
+    public void setGrafickaKartica(String grafickaKartica) {
+        this.grafickaKartica = grafickaKartica;
+    }
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(double cijena) {
+        this.cijena = cijena;
+    }
+
+    public double getVelicinaEkrana() {
+        return velicinaEkrana;
+    }
+
+    public void setVelicinaEkrana(double velicinaEkrana) {
+        this.velicinaEkrana = velicinaEkrana;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public void setHdd(int hdd) {
+        this.hdd = hdd;
+    }
+
+    public int getSsd() {
+        return ssd;
+    }
+
+    public void setSsd(int ssd) {
+        this.ssd = ssd;
+    }
+
+
+}
